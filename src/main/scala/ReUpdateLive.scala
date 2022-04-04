3b362f0cd9439da6c303a3d174e7badc34640146
righ1113 <34955220+righ1113@users.noreply.github.com>
2022-04-05 06:33:35 +0900

    22/04/05 Reduce: ReUpdateLiveクラス の実装2

--- src/main/scala/ReUpdateLive.scala
+++ src/main/scala/ReUpdateLive.scala
@@ -7,12 +7,35 @@ class ReUpdateLive(val rn: TpRingNchar, val ncode: Int, var lTwin: TpLiveTwin):
     lTwin = testMatch.run // computes {\cal M}_{i+1} from {\cal M}_i, updates the bits of "real"
     i += 1
 
-  def isUpdate = false
+  private def isUpdate: Boolean =
+    // runs through "live" to see which colourings still have `real' signed
+    // matchings sitting on all three pairs of colour classes, and updates "live"
+    // accordingly; returns 1 if nlive got smaller and stayed >0, and 0 otherwise *)
+    var newNlive = 0
+    if lTwin.live(0) > 1 then lTwin.live(0) = 15 
+
+    for(i <- 0 until ncode) { if lTwin.live(i) == 15 then { newNlive += 1; lTwin.live(i) = 1 } else lTwin.live(i) = 0 }
+    printf("             %4d", newNlive) // left
+
+    if newNlive < lTwin.nLive && newNlive > 0 then
+      lTwin.nLive = newNlive
+      true
+    else
+      if newNlive == 0 then
+        println("\n\n\n                  ***  D-reducible  ***")
+      else
+        println("\n\n\n                ***  Not D-reducible  ***")
+      lTwin.nLive = newNlive
+      false
 
 
 class TestMatch(val rn: TpRingNchar, var state: TpUpdateState):
   def run: TpLiveTwin =
     println("TestMatch. " + state.real(0).toString)
+    state.nReal    = 0
+    state.byte     = 1
+    state.realterm = 0
+
     state.lTwin
 
 
--- src/main/scala/Reduce.scala
+++ src/main/scala/Reduce.scala
@@ -3,7 +3,7 @@ object Reduce:
     println("Reduce.")
     // UpdateLive (check D reducible)
     val rn    = new TpRingNchar  (8, 0) // nchar = Const::SIMATCHNUMBER[ring] / 8 + 1
-    val lTwin = new TpLiveTwin   (0, Array(1, 2, 3))
+    val lTwin = new TpLiveTwin   (0, Array(1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15))
     val uLive = new ReUpdateLive (rn, 3, lTwin)
     println(uLive.ncode)
     uLive.lTwin.live(0) = 99
